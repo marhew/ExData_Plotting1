e32250c3338b75b8a5d1e20848d5c464ba4ac631
Marcin Hewelt <hewelt@informatik.uni-hamburg.de>
2015-01-11 18:51:27 +0100

    Created plot 1

---
+++ plot1.R
@@ -0,0 +1,10 @@
+colNames <- read.csv("household_power_consumption.txt", 
+                     sep=";", nrows=1)
+powerUse <- read.csv("household_power_consumption.txt", 
+                     sep=";", skip=46*24*60, nrows=3*24*60,
+                     col.names = names(colNames), 
+                     colClasses=c("character","character",rep("numeric",7)))
+powerUse2Days <- powerUse[powerUse$Date %in% c("1/2/2007","2/2/2007"),]
+hist(powerUse2Days$Global_active_power,
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power", col = "red", breaks=15)
