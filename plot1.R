3d2e14b6230f66d14574209901c5eaeb2ae8884e
Marcin Hewelt <hewelt@informatik.uni-hamburg.de>
2015-01-11 21:43:27 +0100

    Code refactoring, added comments

--- plot1.R
+++ plot1.R
@@ -1,10 +1,15 @@
-colNames <- read.csv("household_power_consumption.txt", 
-                     sep=";", nrows=1)
-powerUse <- read.csv("household_power_consumption.txt", 
-                     sep=";", skip=46*24*60, nrows=3*24*60,
-                     col.names = names(colNames), 
+# read only one row to get column names
+colNames <- read.csv("household_power_consumption.txt", sep=";", nrows=1)
+powerUse <- read.csv("household_power_consumption.txt", sep=";", 
+                     skip=46*24*60, # skip the first 46 days
+                     nrows=3*24*60, # read in only 3 days to save RAM
+                     col.names = names(colNames), # use previously read column names
+                     # read Date, Time as character, other columns as numeric
                      colClasses=c("character","character",rep("numeric",7)))
+
+# filter, data only for days 1/2/2007 and 2/2/2007
 powerUse2Days <- powerUse[powerUse$Date %in% c("1/2/2007","2/2/2007"),]
+
 png(filename = "plot1.png", width=480, height=480, bg="white")
 hist(powerUse2Days$Global_active_power,
      xlab = "Global Active Power (kilowatts)",
--- plot4.R
+++ plot4.R
@@ -1,12 +1,19 @@
-colNames <- read.csv("household_power_consumption.txt", 
-                     sep=";", nrows=1)
-powerUse <- read.csv("household_power_consumption.txt", 
-                     sep=";", skip=46*24*60, nrows=3*24*60,
-                     col.names = names(colNames), 
+# read only one row to get column names
+colNames <- read.csv("household_power_consumption.txt", sep=";", nrows=1)
+powerUse <- read.csv("household_power_consumption.txt", sep=";", 
+                     skip=46*24*60, # skip the first 46 days
+                     nrows=3*24*60, # read in only 3 days to save RAM
+                     col.names = names(colNames), # use previously read column names
+                     # read Date, Time as character, other columns as numeric
                      colClasses=c("character","character",rep("numeric",7)))
-powerUse$timestamp <- strptime(paste(powerUse$Date, powerUse$Time), 
-                               format = "%d/%m/%Y %H:%M:%S")
+
+# filter, data only for days 1/2/2007 and 2/2/2007
 powerUse2Days <- powerUse[powerUse$Date %in% c("1/2/2007","2/2/2007"),]
+# add new column 'timestamp' with POSIXct object
+powerUse2Days$timestamp <- strptime(paste(powerUse2Days$Date, 
+                                          powerUse2Days$Time), 
+                               format = "%d/%m/%Y %H:%M:%S")
+
 png(filename = "plot4.png", width=480, height=480, bg="white")
 par(mfrow = c(2,2))
 with(powerUse2Days, {
